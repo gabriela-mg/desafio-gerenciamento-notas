03f672d201d4c9d838ec2b3733be7cd01342db8e
gabriela-mg <gabriela.gandine@4kst.com>
2025-10-16 13:40:29 -0300

    feat: image context react

--- notas-frontend/src/App.tsx
+++ notas-frontend/src/App.tsx
@@ -3,16 +3,12 @@ import './App.css'
 
 import { Route, Routes } from 'react-router';
 import NoteCard from './pages/NoteCard.tsx';
-<<<<<<< HEAD
 import Home from './pages/Home.tsx';
-=======
-import Home from './Home.tsx';
->>>>>>> dev
 
 function App() {        
     return (    
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Home />} />
             <Route path="/note/:id" element={<NoteCard />}/>
         </Routes>
     )
